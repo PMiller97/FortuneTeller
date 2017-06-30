7c01c8803b61c9d6616d0c933b1224fc9c411ba2
Paige Miller <paige_9753@hotmail.com>
2017-06-30 13:04:01 -0400

    Initial Commit

---
+++ FortuneTeller/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to Fortune Teller!");
+
+            Console.WriteLine("Please enter your first name.");
+            string firstName = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter your last name.");
+            string lastName = Console.ReadLine();
+
+
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 0)
+            {
+                Console.WriteLine("Hmm, that does not seem humanly possible. Please try again!");
+
+                return;
+            }
+            else if (age > 0)
+            {
+                Console.WriteLine("Awesome!");
+            }
+
+
+            Console.WriteLine("What is your birth month?");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("What is your favorite ROYGBIV color? If you do not know ROYGBIV, please enter Help.");
+            string userColor = Console.ReadLine();
+            string lowerHelp = userColor.ToLower();
+
+            if (lowerHelp == "help")
+            {
+                Console.WriteLine("Red, Orange, Yellow, Green, Blue, Indigo, Violet.");
+                userColor = Console.ReadLine();
+            }
+
+            Console.WriteLine("Okay, how many siblings do you have?");
+            int siblings = int.Parse(Console.ReadLine());
+
+
+
+
+            
+            
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
